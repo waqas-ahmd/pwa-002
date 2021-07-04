ba7d473c3c6fc7aa78a0975e2ddaacf69f3a4150
waqas-ahmd <65waqas@gmail.com>
2021-07-04 14:45:22 +0500

    initial app

---
+++ script.js
@@ -0,0 +1,28 @@
+document.querySelector("#email-btn").addEventListener("click", () => {
+  window.open("mailto:test@example.com", "_parent");
+});
+document.querySelector("#call-btn").addEventListener("click", () => {
+  window.open("callto:+393519557204", "_parent");
+});
+document.querySelector("#share-btn").addEventListener("click", () => {
+  navigator.share({
+    title: "Share Link",
+    text: "Hello",
+    url: "www.example.com",
+  });
+});
+document.querySelector("#whatsapp-btn").addEventListener("click", () => {
+  window.open(
+    "https://api.whatsapp.com/send?phone=+39%20351%209557204&text=",
+    "_blank"
+  );
+});
+document.querySelector("#appointment-btn").addEventListener("click", () => {
+  window.open("https://calendly.com/medicalaestetic", "_blank");
+});
+// document.querySelector("#play-btn").addEventListener("click", () => {
+//   window.open("/video");
+// });
+// document.querySelector("#gallery-btn").addEventListener("click", () => {
+//   window.open("https://calendly.com/medicalaestetic", "_blank");
+// });
